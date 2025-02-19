bf6c2ee63ce85aa241fa204169f7321374d89e58
Lalit Arvind <lalitarvind.balaji@gmail.com>
2025-02-19 00:41:51 -0700

    modified return type to plaintext

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI, UploadFile, File
 import uvicorn
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+from starlette.responses import PlainTextResponse
+
 import boto3
-import os
-from dotenv import load_dotenv
-load_dotenv()
+
 # pip install python-multipart fastapi "uvicorn[standard]" asyncio
 app = FastAPI()
 
@@ -14,17 +14,12 @@ SDB_DOMAIN_NAME = "1229798494-simpleDB"
 
 s3_client = boto3.client(
                 service_name='s3',
-                region_name='us-east-1',
-                aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
-                aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY") 
+                region_name='us-east-1'
             )
 sdb_client = boto3.client(
                 service_name='sdb',
-                region_name='us-east-1',
-                aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
-                aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY") 
+                region_name='us-east-1'
             )
-# AWS_BUCKET = s3.Bucket(S3_BUCKET_NAME)
 
 executor = ThreadPoolExecutor(max_workers=20)
 
@@ -57,14 +52,12 @@ async def create_upload_file(inputFile: UploadFile = File(...)):
     classification = loop.run_in_executor(executor,get_category,fileName)
     
     result1, result2 = await asyncio.gather(saving,classification)
-    print("result1:",result1)
-    print("result2:",result2)
-    
+
     if "Attributes" in result2 and result2["Attributes"]:
         category = result2["Attributes"][0]["Value"]
     else:
         category = "Unknown"
-    return {fileName:category}
+    return PlainTextResponse(f"{fileName}:{category}")
 
 if __name__=="__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, workers=5,reload=True)
